8e44c3f100793f0edc9d5dc06577690c971ccde6
Hannah ross <hannah.ross51@gmail.com>
2024-09-11 16:02:15 +1200

    added service section

--- src/assets/js/nav.js
+++ src/assets/js/nav.js
@@ -100,3 +100,11 @@ document.addEventListener("keydown", (event) => {
         toggleMenu();
     }
 });
+
+const faqItems = Array.from(document.querySelectorAll('.cs-faq-item'));
+        for (const item of faqItems) {
+            const onClick = () => {
+            item.classList.toggle('active')
+        }
+        item.addEventListener('click', onClick)
+        }
